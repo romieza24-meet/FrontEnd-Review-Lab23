91f7be3dc8b987b83a2b22bf6721e1ca49ff109c
romieza24-meet <139688912+romieza24-meet@users.noreply.github.com>
2023-07-17 16:07:44 +0300

    added bonus

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 
 
 
-function changeBackgroundColor(color) {
-    document.getElementById("body").style.backgroundColor = color;
+function changeBackgroundColor(color1, color2) {
+    let body = document.getElementById("body");
+    
+    if (body.style.backgroundColor === 'rgb(245, 209, 152)')
+    {
+
+        body.style.backgroundColor = color2;
+    }
+    else 
+    {
+        body.style.backgroundColor = color1;
+        console.log(body.style.backgroundColor)
+    }
+   
 }
